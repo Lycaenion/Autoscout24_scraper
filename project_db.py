5a659e854e53fd3f41c417dff23cb5ec530e7905
Linn <linn.martensson@hotmail.se>
2025-08-15 19:38:51 +0200

    functional tables with one-to-many relationship

--- project_db.py
+++ project_db.py
@@ -3,34 +3,38 @@ from datetime import date
 import sqlite3
 
 from sqlalchemy import (
-    String, create_engine, select, delete, ForeignKey
+    String, create_engine, select, delete, ForeignKey, Integer
 )
 from sqlalchemy.orm import (
     DeclarativeBase, Mapped, MappedAsDataclass, mapped_column, Session, relationship, sessionmaker
 )
+from typing import List, Optional
 
 
 class Base(MappedAsDataclass, DeclarativeBase):
     """Subclasses will be converted into dataclasses"""
 
-class Webpages(Base):
+class Webpage(Base):
     """Webpages"""
     __tablename__ = "Webpages"
     __table_args__ = {'sqlite_autoincrement': True}
 
-    id : Mapped[int] = mapped_column(init=False, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(init=False, primary_key=True, autoincrement=True)
     site_name : Mapped[str] = mapped_column(String(50), nullable=False)
-    advertisements : Mapped[int] = relationship('Advertisement', back_populates='Webpages')
+    advertisements: Mapped[Optional[List["Advertisement"]]] = relationship(
+        back_populates="webpage",
+        default_factory=list
+    )
 
-class Advertisements(Base):
+class Advertisement(Base):
 
     __tablename__ = 'Advertisements'
     __table_args__ = {'sqlite_autoincrement': True}
 
-
     id : Mapped[int]= mapped_column(init=False, primary_key=True, autoincrement=True)
     url : Mapped[str]= mapped_column(String(300),nullable=False, unique=True)
     webpage_id : Mapped[int] = mapped_column(ForeignKey('Webpages.id'), nullable=False)
+    webpage : Mapped[Webpage] = relationship(back_populates='advertisements')
     brand: Mapped[str]= mapped_column(String(100),nullable=False)
     model_version: Mapped[str]= mapped_column(String(100),nullable=True)
     year: Mapped[str]= mapped_column(String(100),nullable=True)
@@ -42,7 +46,6 @@ class Advertisements(Base):
     location: Mapped[str]= mapped_column(String(100),nullable=True)
     date_added: Mapped[str]= mapped_column(String(100),default=None, nullable=False, comment='date added to db')
 
-
 def add_to_db(url, brand, model_version, year, price, milage, gearbox, fuel_type, engine_power, location):
     SQLALCHEMY_URI = 'sqlite:///db.sqlite'
     engine = create_engine(SQLALCHEMY_URI, echo=True, echo_pool='debug')
@@ -51,7 +54,7 @@ def add_to_db(url, brand, model_version, year, price, milage, gearbox, fuel_type
     with Session(engine) as session:
         date_added = date.today().isoformat()
 
-        advert = Advertisements(url, brand, model_version, year, price,milage, gearbox, fuel_type, engine_power, location, date_added)
+        advert = Advertisement(url, brand, model_version, year, price,milage, gearbox, fuel_type, engine_power, location, date_added)
         session.add(advert)
         session.commit()
         print('added advertisement')
@@ -61,7 +64,7 @@ def check_if_post_exists_in_db(url):
     engine = create_engine(SQLALCHEMY_URI, echo=True, echo_pool='debug')
 
     with Session(engine) as session:
-        stmt = select(Advertisements).where(Advertisements.url == url)
+        stmt = select(Advertisement).where(Advertisement.url == url)
         result = session.execute(stmt).scalar()
         if result is None:
             print('no advertisement')
@@ -74,5 +77,8 @@ if __name__ == '__main__':
     SQLALCHEMY_URI = 'sqlite:///db.sqlite'
     engine = create_engine(SQLALCHEMY_URI, echo=True, echo_pool='debug')
     Base.metadata.create_all(engine)
+    webpage1 = Webpage(site_name='Bazos')
     Session = sessionmaker(bind=engine)
     session = Session()
+    session.add(webpage1)
+    session.commit()
